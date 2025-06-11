fd1f92defd2d3f945161181987f4202b82554fe7
VJ <basarkod@gmail.com>
2025-06-11 10:49:46 -0400

    added gh pages config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+// App.js
+
 import './App.css';
+import CountryInformation from './components/CountryInformation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CountryInformation />
     </div>
   );
 }
